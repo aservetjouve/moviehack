126810b355001f3b66f5b14a2d97bc9a6ef6b10b
Andy Servet Jouve <aservetjouve@gmail.com>
2020-06-05 12:16:28 +0200

    add views files, basic sign in and sign up routes, install handlebars

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ greeting: process.env.GREETING });
-});
+const hbs = require('hbs');
+
+// Template engine
+app.set('view engine', 'hbs');
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routers
+const authRouter = require('./routes/auth.routes');
+
+// Routes middleware
+app.use('/', authRouter);
 
 const port = process.env.PORT || 8000;
 
